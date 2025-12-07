73ea7400d27191b8e72d8a7a001ce63d74a41dab
rotarualexandru22 <rotarualexandru22@yahoo.com>
2025-12-07 13:37:39 +0200

    Fixed: Contact form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -11,7 +11,7 @@ const ContactUs = () => {
 
         const formData = new FormData(event.target);
 
-        formData.append("access_key", import.meta.env.VITE_WEB3FORM_KEY);
+        formData.append("access_key", import.meta.env.VITE_WEB3FORMS_KEY);
 
         try {
             const response = await fetch("https://api.web3forms.com/submit", {
